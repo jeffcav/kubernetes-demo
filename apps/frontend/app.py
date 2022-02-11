37f192041b3eb41a86d60e553d6dcbb5670f5503
Jefferson Cavalcante <recife.rodrigo@gmail.com>
2022-02-11 14:07:40 -0300

    Improve frontend app

--- apps/frontend/app.py
+++ apps/frontend/app.py
@@ -9,8 +9,10 @@ def get_data():
     time_server = os.environ.get('TIME_SERVER')
     
     current_time = requests.get("http://" + time_server).text
-
-    html = "<html><body style=\"background-color:powderblue;\"><h1>Current time: {}</h1></body></html>\n".format(current_time)
-
+    
+    html = (
+        "<html><body style=\"background-color:powderblue; font-size: 30px;\">"
+        f"Time now: {current_time}"
+        "</body></html>\n")
 
     return html
